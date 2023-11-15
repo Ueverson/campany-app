6ce062c3a37b24404552973b8d2ede90e171ccda
Francine Nogueira <francine.pns@gmail.com>
2023-11-15 19:04:29 -0300

    Criação de estilo para a página de solicitação de compra

---
+++ src/pages/css/solicitacaoCompraStyles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const solicitacaoCompraStyles = StyleSheet.create({
+  container: {
+    backgroundColor: '#232020',
+    flex: 1,
+  },
+  textWhite: {
+    color: 'white'
+  },
+  pageTitle: {
+    marginTop: 20,
+    fontSize: 20, 
+    color: 'white'
+  },
+  title: {
+    color: 'black',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
+
+export default solicitacaoCompraStyles;
