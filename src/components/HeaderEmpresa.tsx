c3b3db1b7ba0f01969e7c1fa715603397929d38c
Francine Nogueira <francine.pns@gmail.com>
2023-11-15 16:16:12 -0300

    Modificação no estilo para o header da empresa

--- src/components/HeaderEmpresa.tsx
+++ src/components/HeaderEmpresa.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import headerStyles from './css/headerStyles';
 
-function HeaderEmpresa({ to, icon }: any) {
+const HeaderEmpresa = ({ to, icon }: any) => {
   const navigation = useNavigation<any>();
 
   return (
-    <View style={{ alignItems:'center' ,marginTop: 20 }}>
-      <View style={{ flexDirection: 'row'}}>
-        <TouchableOpacity onPress={() => navigation.navigate(to)}>
-          <View className="text-orange-500 font-bold justify-center items-center">
-            {icon}
-          </View>
-        </TouchableOpacity>
-        <Text className="text-orange-500 text-4xl font-bold justify-center">Rockland Bar</Text>
+    <View style={headerStyles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate(to)}>
+        <View style={headerStyles.iconContainer}>
+          {icon}
+        </View>
+      </TouchableOpacity>
+      <View style={headerStyles.titleContainer}>
+        <Text style={headerStyles.titleText}>Rockland Bar</Text>
       </View>
-      <Text className="text-white-300 text-xl justify-center font-bold">Bem-vindo!</Text>
     </View>
-
   );
 }
 
